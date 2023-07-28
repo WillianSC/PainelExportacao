b577e5eb7b8b28b3f55360ff94446980adbdd394
Willian Santiago Calestini <williancalestini-sgk@corporate.ad>
2023-07-28 09:39:53 -0300

    Ajuste do diretório

--- PainelExportacao.py
+++ PainelExportacao.py
@@ -1,5 +1,8 @@
-from asyncio import sleep
+import tkinter as tk
+from tkinter import simpledialog
 from datetime import datetime
+from dateutil.parser import parse
+from asyncio import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,8 +11,13 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 import time
+import logging
+
+logging.basicConfig(filename='Painel_Exportacao.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def login(driver):
+    logging.info("Realizando Login")
     usuario = driver.find_element(By.XPATH, "/html/body/div/div[2]/form/div[1]/label[1]/input")
     usuario.send_keys("wcalestini")
     usuario.send_keys(webdriver.Keys.TAB)
@@ -19,11 +27,13 @@ def login(driver):
     time.sleep(1)
 
 def painel(driver):
+    logging.info("Clicando no Painel Reduzao Z")
     clickpainel = driver.find_element(By.XPATH, '/html/body/div/div[2]/nav/ul/div/a/p')
     clickpainel.click()
     time.sleep(1)
 
 def preencher_lojas(driver, loja):
+    logging.info(f"Preenchendo Informação de Loja: {loja}")
     lojaInicial = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[1]/input")
     lojaInicial.clear()
     time.sleep(1)
@@ -37,21 +47,40 @@ def preencher_lojas(driver, loja):
     time.sleep(1)
 
 def limpa_filtro_loja(driver):
+    logging.info("Limpando Filtro Loja Inicial e Final")
     lojaInicial = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[1]/input")
     lojaInicial.clear()
     lojaFinal = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[2]/input")
     lojaFinal.clear()
 
 def limpa_filtro_pdv(driver):
+    logging.info("Limpando Filtro PDV Inicial e Final")
     pdvInicial = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[5]/input")
     pdvInicial.clear()
     pdvFinal = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[6]/input")
     pdvFinal.clear()
 
-def converter_datas(driver):
-    data_inicial_str = "2023-07-20"
+def convert_date_format(date_string):
+    formats_to_try = ["%d-%m-%Y", "%d/%m/%Y", "%d-%m-%Y"]
+    for fmt in formats_to_try:
+        try:
+            date_obj = datetime.strptime(date_string, fmt)
+            return date_obj.strftime("%Y-%m-%d")
+        except ValueError:
+            pass
+    return None
+
+def preencher_data_processamento(driver, data_processamento):
+    logging.info(f"Data de processamento: {data_processamento}")
+    data_processamento_element = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[3]/input")
+    data_processamento_element.clear()
+    data_processamento_element.send_keys(data_processamento)
+
+def converter_datas(driver, data_processamento):
+    logging.info("Convertendo Data inserida")
+    data_inicial_str = data_processamento
     data_inicial = datetime.strptime(data_inicial_str, "%Y-%m-%d").date()
-    data_final_str = "2023-07-20"
+    data_final_str = data_inicial_str
     data_final = datetime.strptime(data_final_str, "%Y-%m-%d").date()
 
     data_inicial_element = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[3]/input")
@@ -62,6 +91,7 @@ def converter_datas(driver):
     data_final_element.send_keys(data_final.strftime("%d/%m/%Y"))
 
 def preencher_pdvs(driver, pdv):
+    logging.info(f"Preenchendo Informação de PDV: {pdv}")
     pdvInicial = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[5]/input")
     pdvInicial.clear()
     time.sleep(1)
@@ -72,6 +102,7 @@ def preencher_pdvs(driver, pdv):
     pdvFinal.send_keys(pdv)
 
 def ativar_checkboxes(driver):
+    logging.info("Ativando CheckBox's")
     filtroIntegrar = driver.find_element(By.NAME, "TODO")
     filtroIntegrar.click()
     filtroIntegrado = driver.find_element(By.NAME, "DONE")
@@ -91,16 +122,20 @@ def ativar_checkboxes(driver):
     time.sleep(1)
 
 def filtrar(driver):
+    logging.info("Clicando botão Filtrar")
     botaoFiltrar = driver.find_element(By.XPATH, "/html/body/div/div[2]/div/div/div[3]/form/div[10]/button")
     botaoFiltrar.click()
     time.sleep(1)  # Pequeno atraso para aguardar a ação concluir
 
 def reintegra_cupom(driver, soup):
+    logging.info("Ação de REINTEGRAR Cupom")
     # Valida se a página não possui cupom:
     empty_message_div = soup.find('div', class_='ColorEmptyMessage')
     if empty_message_div:
+        logging.info("Campo Cupom Vazio, Pulando para o próximo PDV")
         pass
     else:
+        logging.info("Selecionando todos Cupons")
         # Clicar no switch "Selecionar toda pagina"
         botao_selecionar = driver.find_element(By.CSS_SELECTOR, '.react-switch-handle')
         actions = ActionChains(driver)
@@ -110,19 +145,23 @@ def reintegra_cupom(driver, soup):
 
         # Verifica se o elemento "Clique Aqui!" está presente na página
         try:
+            logging.info("Clicando no painel Clique aqui")
             message_element = driver.find_element(By.CSS_SELECTOR, 'div.message')
             actions = ActionChains(driver)
             actions.click(message_element)
             actions.perform()
             time.sleep(1)  # Pequeno atraso para aguardar a ação concluir
         except NoSuchElementException:
+            logging.info("Painel Clique Aqui não encontrado")
             pass  # Se não encontrar o elemento "Clique Aqui!", continua o fluxo
+
+        logging.info("Clicando no botão Reintegrar")
         # Botão Reintegrar
         botaoReintegrar = driver.find_element(By.XPATH,'/html/body/div/div[2]/div/div/div[1]/div/button/p')
         botaoReintegrar.click()
 
-
 def main():
+    logging.info("Abrindo Navegador")
     # Abre o Navegador
     driver_path = "C:\ChromeDriver\chromedriver.exe"
     driver = webdriver.Chrome()
@@ -157,6 +196,20 @@ def main():
         {"loja": "5284", "pdvs": ["1", "2", "3", "4"]}
     ]
 
+    # Solicitar a data de processamento ao usuário
+    root = tk.Tk()
+    root.withdraw()
+    while True:
+        data_processamento = simpledialog.askstring("Data de Processamento", "Qual a data de processamento (DD-MM-YYYY)?")
+        if data_processamento:
+            converted_date = convert_date_format(data_processamento)
+            if converted_date:
+                break
+            else:
+                logging.error("Formato de data inválido. Tente novamente.")
+        else:
+            logging.error("Nenhuma data inserida. Tente novamente.")
+    
     # Loop para iterar sobre as lojas e PDVs
     for item in lojas_pdvs:
         loja = item["loja"]
@@ -166,7 +219,7 @@ def main():
         limpa_filtro_loja(driver) # Limpa filtro Loja
         limpa_filtro_pdv(driver) # Limpa filtro PDV
         preencher_lojas(driver, loja)  # Preenchendo informação de Loja Inicial e Final
-        converter_datas(driver) # Insere data no campo
+        converter_datas(driver, converted_date) # Insere data no campo
         ativar_checkboxes(driver)  # Ativa os checkboxes
 
         # Loop para iterar sobre os PDVs
